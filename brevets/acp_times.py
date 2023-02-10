a9e0510b4afdb6af0e1206cba41b5cc44960bcd2
kester <kester@ubtunu.myguest.virtualbox.org>
2023-02-10 14:42:39 -0800

    Started

--- brevets/acp_times.py
+++ brevets/acp_times.py
@@ -3,8 +3,12 @@ Open and close time calculations
 for ACP-sanctioned brevets
 following rules described at https://rusa.org/octime_alg.html
 and https://rusa.org/pages/rulesForRiders
+
+Algorithm
+https://rusa.org/pages/acp-brevet-control-times-calculator
 """
 import arrow
+import math
 
 
 #  You MUST provide the following two functions
@@ -15,7 +19,7 @@ import arrow
 
 
 def open_time(control_dist_km, brevet_dist_km, brevet_start_time):
-    """
+   """
     Args:
        control_dist_km:  number, control distance in kilometers
        brevet_dist_km: number, nominal distance of the brevet
@@ -25,8 +29,19 @@ def open_time(control_dist_km, brevet_dist_km, brevet_start_time):
     Returns:
        An arrow object indicating the control open time.
        This will be in the same time zone as the brevet start time.
-    """
-    return arrow.now()
+
+       34 km/hr max speed 0-200
+       ctrl loc.    max sp.   min sp. 
+       0-200        34         15
+       200-400      32         15
+       400-600      30         15
+       600 - 1000   28         11.428
+       1000-1300    26         13.333
+   """
+   temp = control_dist_km / 34
+   hours = math.floor(temp)
+   minutes = (temp - hours) * 60
+   return arrow.now()
 
 
 def close_time(control_dist_km, brevet_dist_km, brevet_start_time):
